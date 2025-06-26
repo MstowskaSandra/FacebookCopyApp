3caee6dcb86805ff4f5b3d8ca0e9f8c428e07fa5
Sandra <sandra.mstowskaa@gmail.com>
2025-06-26 15:20:13 +0200

    Add Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useSelector } from "react-redux";
-import { formatDistanceToNow } from 'date-fns';
+
 import Navbar from "./Components/Navbar";
+import Post from "./Components/Post/Post";
 
 
 function App() {
-  const posts = useSelector((state) => state.posts.posts);
-  const users = useSelector((state) => state.users.users);
-  
+
   return (
     <>
       <Navbar />
@@ -23,41 +21,7 @@ function App() {
             alt="Search"
             className="px-3 py-1 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 "
             />
-          <div className="flex flex-col gap-6">
-            {posts.map((post) => {
-              const author = users.find((u) => u.id === post.userId);
-               return (
-                  <div key={post.id} className="border border-gray-200 rounded-xl p-6 bg-white shadow-sm space-y-4 ">
-                    <div className="flex items-center gap-2 mb-2">
-                      <img src={author.avatar} alt={author.name} className="w-10 h-10 rounded-full" />
-                      <div className="flex flex-col">
-                        <span className="font-bold">{author.name}</span>
-                        <span className="text-sm text-gray-500">
-                          {formatDistanceToNow(new Date(post.createdAt), { addSuffix: true })}
-                        </span>
-                      </div>  
-                    </div>
-                    <p>{post.content}</p>
-
-                    {post.image && (
-                    <img src={post.image} alt="post image" className="w-full max-h-96 object-cover rounded-lg shadow-sm" 
-                    />
-                    )}
-                    
-                    <div className="flex items-center gap-2">
-                      <div  className="flex items-center gap-1">
-                        <img src={`${process.env.PUBLIC_URL}/Assets/Icons/fb-thumb-up.png`} alt="like icon" className="w-5 h-5"/>
-                        <span className="text-sm text-gray-600">{post.likes.length}</span>
-                      </div>
-                      <div className="flex items-center gap-1">
-                        <img src={`${process.env.PUBLIC_URL}/Assets/Icons/fb-comment.png`} alt="comment icon" className="w-5 h-5"/>
-                        <span className="text-sm text-gray-600">{post.comments.length}comments</span>
-                      </div>
-                    </div>  
-                  </div>
-                );
-            })}
-          </div>
+            <Post />
         </div>
         <div className="flex flex-col gap-4 border p-4 bg-white rounded-lg shadow">
           <div className="border p-4 rounded">.</div>
---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { formatDistanceToNow } from 'date-fns';
+
+const Post = () => {
+    const posts = useSelector((state) => state.posts.posts);
+    const users = useSelector((state) => state.users.users);
+  return (
+    <div className="flex flex-col gap-6">
+        {posts.map((post) => {
+            const author = users.find((u) => u.id === post.userId);
+            return (
+                <div key={post.id} className="border border-gray-200 rounded-xl p-6 bg-white shadow-sm space-y-4 ">
+                    <div className="flex items-center gap-2 mb-2">
+                        <img src={author.avatar} alt={author.name} className="w-12 h-12 rounded-full" />
+                        <div className="flex flex-col">
+                            <span className=" font-bold">{author.name}</span>
+                            <span className="text-sm text-gray-500">
+                                {formatDistanceToNow(new Date(post.createdAt), { addSuffix: true })}
+                            </span>
+                        </div>  
+                    </div>
+                    <p>{post.content}</p>
+
+                    {post.image && (
+                    <img src={post.image} alt="post image" className="w-full max-h-96 object-cover rounded-lg shadow-sm" />
+                    )}
+                    
+                    <div className="flex items-center gap-6">
+                        <div  className="flex items-center gap-1">
+                            <img src={`${process.env.PUBLIC_URL}/Assets/Icons/fb-thumb-up.png`} alt="like icon" className="w-5 h-5"/>
+                            <span className="text-sm text-gray-600">{post.likes.length}</span>
+                        </div>
+                        <div className="flex items-center gap-1">
+                            <img src={`${process.env.PUBLIC_URL}/Assets/Icons/fb-comment.png`} alt="comment icon" className="w-5 h-5"/>
+                            <span className="text-sm text-gray-600">{post.comments.length}comments</span>
+                        </div>
+                    </div>  
+                </div>
+            );
+        })}
+    </div>
+  );
+};
+
+export default Post;
